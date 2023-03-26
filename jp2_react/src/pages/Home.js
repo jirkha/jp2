2e95e063626f166a56a298a8dcb1176551b976f9
jirkha <jirkha13@seznam.cz>
2023-03-26 11:38:41 +0200

    background image on home page

--- jp2_react/src/pages/Home.js
+++ jp2_react/src/pages/Home.js
@@ -1,71 +1,110 @@
-import { Box, Link, Stack, Typography } from "@mui/material";
-import img from "../assets/j&p_img_candle.jpg";
+import { Box, Card, Link, Paper, Stack, Typography } from "@mui/material";
+import img from "../assets/jp_img_candle_opacity3.jpg";
 import img2 from "../assets/pistacie1-8.jpg";
 import img3 from "../assets/vosk_krabicka.jpg";
 
 export default function Home() {
+
   return (
     <div>
-      <Typography color="primary" variant="h4" sx={{ mt: 4 }}>
-        Vítejte ve zkušební verzi
-      </Typography>
-      <Typography
-        variant="h2"
-        color="primary"
-        //align="center" //zarovná doprostřed
-        gutterBottom //vytvoří mezeru pod textem
+      <Paper
+        sx={{
+          backgroundImage: `url(${img})`,
+          //backgroundSize:"cover",
+          m: 0,
+          minHeight: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          // opacity: 0.3,
+          // position: "relative",
+          //display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: -1,
+        }}
       >
-        <strong>Podnikový systém</strong>
-      </Typography>
-      <Typography color="primary" variant="h5" sx={{ mt: 2 }}>
-        Pokračujte výběrem libovolné sekce a zkuste přidat materiál, produkt,
-        prodejní kanál. Poté můžete vložit transakce, podívat se na statistiku a
-        další.
-      </Typography>
-      <Typography color="primary" variant="h5" sx={{ mt: 3 }}>
-        Pokud se chcete dozvědět více o tvůrci této aplikace, navštivte {" "}
-        <a href="https://www.jirivecko.cz">www.jirivecko.cz</a>.
-        {/* <Link sx={{ ml: 1 }} 
-        //href="/aboutme"
-        href="/aboutme#/aboutme/"
+        <Typography color="primary" variant="h4" sx={{ p: 2, pt:6 }}>
+          <span style={{ backgroundColor: "aliceblue" }}>
+            Vítejte ve zkušební verzi
+          </span>
+        </Typography>
+
+        <Typography
+          variant="h2"
+          color="primary"
+          sx={{ p: 2 }}
+          //align="center" //zarovná doprostřed
+          gutterBottom //vytvoří mezeru pod textem
         >
-          ZDE
-        </Link> */}
-      </Typography>
-      <Stack
-        direction={{ sm: "column", md: "row" }}
-        spacing={{ xs: 1, sm: 2, md: 6 }}
-        sx={{ mt: 3 }}
-        justifyContent="space-between"
-      >
-        <Box
-          height="100%"
-          display="flex"
-          justifyContent="center"
-          flexDirection="column"
+          <span style={{ backgroundColor: "aliceblue" }}>
+            <strong>Podnikový systém</strong>
+          </span>
+        </Typography>
+        <Typography color="primary" variant="h5" sx={{ p: 2 }}>
+          <span style={{ backgroundColor: "aliceblue" }}>
+            Pokračujte výběrem libovolné sekce a zkuste přidat materiál,
+            produkt, prodejní kanál. Poté můžete vložit transakce, podívat se na
+            statistiku a další.
+          </span>
+        </Typography>
+        <Typography color="primary" variant="h5" sx={{ p: 2 }}>
+          <span style={{ backgroundColor: "aliceblue" }}>
+            Pokud se chcete dozvědět více o tvůrci této aplikace, navštivte{" "}
+            <a href="https://www.jirivecko.cz">www.jirivecko.cz</a>.
+          </span>
+        </Typography>
+        <Stack
+          direction={{ sm: "column", md: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 6 }}
+          sx={{ mt: 3 }}
+          justifyContent="space-between"
         >
-          <img src={img3} width="261" height="191" alt="img3" />
-        </Box>
-        <Box
-          height="100%"
-          display="flex"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <img src={img} width="261" height="191" alt="img" />
-        </Box>
-        <Box
-          height="100%"
-          display="flex"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <img src={img2} width="261" height="191" alt="img2" />
-        </Box>
-      </Stack>
-      <Typography variant="h6" sx={{ mt: 6, color: "error.main" }}>
-        Verze slouží pouze k prezentačním účelům
-      </Typography>
+          {/* <Box
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <img src={img3} width="261" height="191" alt="img3" />
+          </Box>
+          <Box
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <img src={img} width="261" height="191" alt="img" />
+          </Box>
+          <Box
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <img src={img2} width="261" height="191" alt="img2" />
+          </Box> */}
+        </Stack>
+        <Typography variant="h6" sx={{ p: 2, color: "error.main" }}>
+           <span style={{ backgroundColor: "aliceblue" }}>Verze slouží pouze k prezentačním účelům</span>
+        </Typography>
+      </Paper>
+      {/* <Box
+        component="img"
+        src={img}
+        //sx={{ width: "4%", minWidth: 55 }}
+        sx={{
+          m: 0,
+          opacity: 0.3,
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: -1,
+          height: "100vh",
+        }}
+        //width="55px"
+        alt="Logo"
+      ></Box> */}
     </div>
   );
 }
