5a1cb17a29b1e762c649e6e311c07db4b2628f99
jirkha <jirkha13@gmail.com>
2025-06-28 12:14:29 +0200

    Edit About component

--- jp2_react/src/components/Global/Other/About.js
+++ jp2_react/src/components/Global/Other/About.js
@@ -1,11 +1,23 @@
-import React from "react";
 import portrait from "../../../assets/portrait_jiri_vecko_cut.jpg";
 import github from "../../../assets/images/Github.png";
 import jiri_vecko from "../../../assets/jiri_vecko_logo_sm.jpeg";
 import linkedIn from "../../../assets/images/LinkedIn.png";
-import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import { Box, Container, Divider, Stack, Typography, Chip } from "@mui/material";
 
 function About() {
+
+  const techStack = [
+    "React",
+    "JavaScript",
+    "Python",
+    "Django",
+    "PostgreSQL",
+    "REST API",
+    "Material UI",
+    "Axios",
+    "Redux",
+  ];
+
   return (
     <Container maxWidth="lg">
       <Stack
@@ -32,14 +44,31 @@ function About() {
           flexDirection="column"
           alignItems="center"
         >
-          <img src={portrait} width="222" alt="Portrait" />
+          <Box
+            component="img"
+            src={portrait}
+            alt="Portrait"
+            sx={{
+              width: {
+                xs: "180px",
+                sm: "250px",
+              },
+              height: "auto",
+            }}
+          />
         </Box>
+
         <Container>
           <Typography
-            variant="h4"
+            variant="h5"
             color="#004C99"
             sx={{
               textAlign: "center",
+              letterSpacing: {
+                xs: "0em",
+                sm: "0.025em",
+                md: "0.05em",
+              }, // em je relativní jednotka vůči velikosti písma, doporučená pro responsivní text
               "@media (min-width: 600px)": {
                 textAlign: "left",
               },
@@ -48,12 +77,20 @@ function About() {
             <strong>Jiří Vecko</strong>
           </Typography>
 
-          <Stack direction={{ xs: "column", md: "row" }} sx={{ py: 1 }}>
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            sx={{ py: { xs: 0, md: 1 } }}
+          >
             <Typography
-              variant="h6"
+              variant="subtitle1"
               color="success"
               sx={{
                 textAlign: "center",
+                letterSpacing: {
+                  xs: "0em",
+                  sm: "0.025em",
+                  md: "0.05em",
+                },
                 pb: 1,
                 "@media (min-width: 600px)": {
                   textAlign: "left",
@@ -62,7 +99,7 @@ function About() {
                 pr: 2,
               }}
             >
-              Full-Stack Developer
+              <strong>Full-Stack Developer</strong>
             </Typography>
 
             <Stack
@@ -77,36 +114,60 @@ function About() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={jiri_vecko} alt="GitHub Logo" height="30" />
+                <img src={jiri_vecko} alt="GitHub Logo" height="25" />
               </a>
               <a
                 href="https://github.com/jirkha/jp2"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={github} alt="GitHub Logo" height="30" />
+                <img src={github} alt="GitHub Logo" height="25" />
               </a>
               <a
                 href="https://www.linkedin.com/in/ji%C5%99%C3%AD-vecko-44579b127/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={linkedIn} alt="GitHub Logo" height="30" />
+                <img src={linkedIn} alt="GitHub Logo" height="25" />
               </a>
             </Stack>
           </Stack>
 
           <Typography
-            variant="subtitle1"
+            variant="body2"
             sx={{
               textAlign: "center",
+              letterSpacing: {
+                xs: "0em",
+                sm: "0.025em",
+                md: "0.05em",
+              },
               "@media (min-width: 600px)": {
                 textAlign: "left",
               },
             }}
           >
-            Mou největší motivací je touha učit se novým věcem. Proto jsem v
-            roce 2021 začal s programováním a neustále se posouvám vpřed.
+            Vítejte v demo verzi aplikace, kterou jsem vytvořil pro drobného
+            podnikatele. Umožňuje evidovat a spravovat materiál včetně
+            vyskladnění a naskladnění, produkty, prodejní kanály, transakce a
+            statistiku tržeb.
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: 1,
+                justifyContent: "center",
+                marginTop: 1,
+                "@media (min-width: 600px)": {
+                  justifyContent: "flex-start",
+                  marginTop: 2,
+                },
+              }}
+            >
+              {techStack.map((tech) => (
+                <Chip key={tech} label={tech} color="primary" size="small" />
+              ))}
+            </Box>
           </Typography>
         </Container>
       </Stack>
