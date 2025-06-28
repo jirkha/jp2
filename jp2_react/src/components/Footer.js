807bfb3c90921c83e3a6ec1fca15524760c9deb7
jirkha <jirkha13@gmail.com>
2025-06-28 12:44:31 +0200

    Fix: run build after Edit About component

--- jp2_react/src/components/Footer.js
+++ jp2_react/src/components/Footer.js
@@ -11,10 +11,16 @@ const Footer = () => {
       <Stack
         component="footer"
         direction={{ xs: "column", sm: "row" }}
-        sx={{ justifyContent: "space-evenly", alignItems: "center", p: 1, pt: 4 }}
+        sx={{
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          p: 1,
+          pt: 4,
+        }}
       >
         <Typography variant="h6" color="primary">
-          © Jiří Vecko
+          © 2022–{new Date().getFullYear()} Jiří Vecko. Všechna práva
+          vyhrazena.
         </Typography>
         <Box sx={{ display: { xs: "none", md: "block" } }}>
           <a href="https://www.jirivecko.cz">
@@ -27,19 +33,14 @@ const Footer = () => {
           <Box
             component="img"
             src={jirivecko}
-            sx={{ p: 2}}
+            sx={{ p: 2 }}
             width="45px"
             alt="Logo"
           />
         </a>
 
         <a href="https://www.jpcandles.cz">
-          <Box
-            component="img"
-            src={logo}
-            height="45px"
-            alt="Logo"
-          />
+          <Box component="img" src={logo} height="45px" alt="Logo" />
         </a>
       </Stack>
     </>
