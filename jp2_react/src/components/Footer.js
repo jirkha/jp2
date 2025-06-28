27552a321fdf58416f14d60accf5df129bca6a01
jirkha <jirkha13@gmail.com>
2025-06-28 13:15:09 +0200

    Edit Footer

--- jp2_react/src/components/Footer.js
+++ jp2_react/src/components/Footer.js
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
 import { Stack, Typography, Box, Divider } from "@mui/material";
 import logo from "../assets/JP_logo_black.png";
 import jirivecko from "../assets/jiri_vecko_logo_sm.jpeg";
@@ -7,18 +5,42 @@ import jirivecko from "../assets/jiri_vecko_logo_sm.jpeg";
 const Footer = () => {
   return (
     <>
-      {/* <Divider sx={{ mt: 5 }} /> */}
+      <Divider sx={{ mt: 4 }} />
       <Stack
         component="footer"
         direction={{ xs: "column", sm: "row" }}
-        sx={{ justifyContent: "space-evenly", alignItems: "center", p: 1, pt: 4 }}
+        sx={{
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          p: 1,
+          pt: 2,
+          textAlign: { xs: "center", sm: "left" },
+        }}
       >
-        <Typography variant="h6" color="primary">
-          © Jiří Vecko
+        <Typography
+          variant="h6"
+          color="primary"
+          sx={{
+            textAlign: { xs: "center", sm: "left" },
+          }}
+        >
+          © 2022–{new Date().getFullYear()} Jiří Vecko. Všechna práva vyhrazena.
         </Typography>
-        <Box sx={{ display: { xs: "none", md: "block" } }}>
+
+        <Box
+          sx={{
+            display: { xs: "none", md: "block" },
+          }}
+        >
           <a href="https://www.jirivecko.cz">
-            <Typography variant="h6">www.jirivecko.cz</Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                textAlign: { xs: "center", sm: "left" },
+              }}
+            >
+              www.jirivecko.cz
+            </Typography>
           </a>
         </Box>
 
@@ -27,19 +49,14 @@ const Footer = () => {
           <Box
             component="img"
             src={jirivecko}
-            sx={{ p: 2}}
+            sx={{ p: 2 }}
             width="45px"
             alt="Logo"
           />
         </a>
 
         <a href="https://www.jpcandles.cz">
-          <Box
-            component="img"
-            src={logo}
-            height="45px"
-            alt="Logo"
-          />
+          <Box component="img" src={logo} height="45px" alt="Logo" />
         </a>
       </Stack>
     </>
